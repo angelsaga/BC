bd9a78085bc7e3466c6a247471bff6098630b72b
angelaga <angelaga@163.com>
2017-12-20 23:28:02 +0800

    make photo responsive II

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,22 +64,22 @@ export class AppComponent {
 
     this.photo_list = [{
       photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
-      photo_des : "Description Description Description Description Description Description Description Description"
+      photo_des : "Description Description Description Description"
     },{
       photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
       photo_des : "Description Description Description Description"
     },{
       photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
-      photo_des : "Description Description Description Description Description Description Description Description"
-    },{
-      photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
-      photo_des : "Description Description Description Description Description Description Description Description"
+      photo_des : "Description Description Description Description"
     },{
       photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
       photo_des : "Description Description Description Description"
     },{
       photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
-      photo_des : "Description Description Description Description Description Description Description Description"
+      photo_des : "Description Description Description Description"
+    },{
+      photo_url : "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3101860224,2352777645&fm=173&s=C23014C7000912EE141CBDA403007013&w=218&h=146&img.JPEG",
+      photo_des : "Description Description Description Description"
     },]
   }
 
